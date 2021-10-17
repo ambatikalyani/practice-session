fc9662f9cbb078bb26dc966f7cb401ca345b4657
18_301 Kalyani Kalyani <92663963+ambatikalyani@users.noreply.github.com>
2021-10-17 07:43:50 +0000

    userAge

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-var readlinesync = require('readline-sync');
-var userName = readlinesync.question("what is ur name?");
-console.log(userName);
-console.log("welcome " + userName);
+var readlineSync = require ("readline-sync");
+var userAnswerAge = readlineSync.quesion("r u older than 25?");
+ console.log(userAge);
+if (userAge === "yes"){
+   console.log("u r right");
+}else{
+  console.log("u r wrong");
+}
